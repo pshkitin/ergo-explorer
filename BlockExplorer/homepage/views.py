1ff7db24915986b0e1a564cda93e5cc711b3ee86
dundro <popovva@pochtabank.ru>
2018-04-07 14:41:05 +0300

    adds block list

--- BlockExplorer/homepage/views.py
+++ BlockExplorer/homepage/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+import requests
 # from django.Http import HttpResponse
 # Create your views here.
 
+
 def homepage(request):
-    return render(request, 'homepage/index.html')
+    a = requests.get("http://88.198.13.202:9051/blocks?offset=6&&limit=10")
+    blocklist = a.json()
+    # print(blocklist)
+    return render(request, 'homepage/index.html', context={"blocklist": blocklist})
